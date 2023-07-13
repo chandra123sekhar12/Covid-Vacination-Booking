04f620efc2017cbc51dc87d5ee42eaad5fc0762a
chandra123sekhar12 <127502272+chandra123sekhar12@users.noreply.github.com>
2023-07-13 09:48:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const app=express();
 var corsOptions={
-    origin:"http://localhost:1439"
+    origin:"https://covid-vacination-booking.vercel.app"
 };
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 const db = require("./app/models");
 db.mongoose
-.connect(db.url)
+.connect(mongodb+srv://dbuser:Amulya@14@cluster0.0ocf6oh.mongodb.net/cv_db?retryWrites=true&w=majority)
 .then(()=>{
     console.log("Connected to the database!")
 })
